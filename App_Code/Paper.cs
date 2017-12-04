0599e831184b3c046c3e8619740445b9e2ff9106
andyb <andyb@LAPTOP-BRC70Q4T>
2017-12-04 16:22:05 -0500

    Classes Added

---
+++ App_Code/Paper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Paper
+/// </summary>
+public class Paper
+{
+    private int id;
+    private int authorID;
+    private int confID;
+    private string title;
+    private string path;
+    
+    public Paper(int id, int authorID, int confID, string title, string path)
+    {
+        this.id = id;
+        this.authorID = authorID;
+        this.confID = confID;
+        this.title = title;
+        this.path = path;
+    }
+
+    public void submitPaper(string localPath)
+    {
+
+    }
+
+    public String[] getPapersForConf(int confID)
+    {
+        return papersForConf
+    }
+
+    public void viewPaper(int id)
+    {
+
+    }
+}
---
+++ App_Code/Registration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Registration
+/// </summary>
+public class Registration
+{
+    int id;
+    int userID;
+    int confID;
+    int privilege; 
+    
+    public Registration(int id, int userID, int confID, int privilege)
+    {
+        this.id = id;
+        this.userID = userID;
+        this.confID = confID;
+        this.privilege = privilege;
+    }
+
+    public void register(int userID, int confID)
+    {
+
+    }
+
+    public void setPrivilege(int userID, int privilege)
+    {
+            
+    }
+}
